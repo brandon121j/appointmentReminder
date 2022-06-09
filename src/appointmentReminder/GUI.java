eebe2e58c61d5ca94850b10511e2e1097821e6c7
Brandon Johnson <brandon121j@gmail.com>
2022-06-09 19:56:34 -0400

    First Commit

--- src/appointmentReminder/GUI.java
+++ src/appointmentReminder/GUI.java
@@ -8,7 +8,7 @@ public class GUI {
 	
 	public static void main(String[] args) {
 		JPanel panel = new JPanel();
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("Appointment Reminder");
 		frame.setSize(400, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -16,13 +16,22 @@ public class GUI {
 		
 		panel.setLayout(null);
 		
-		JLabel label = new JLabel("Therapist Name");
-		label.setBounds(10, 20, 100, 25);
-		panel.add(label);
+		JLabel therapistLabel = new JLabel("Therapist Name");
+		therapistLabel.setBounds(10, 20, 100, 25);
+		panel.add(therapistLabel);
 		
-		JTextField userText = new JTextField(20);
-		userText.setBounds(100, 20, 165, 25);
-		panel.add(userText);
+		JTextField therapistText = new JTextField(20);
+		therapistText.setBounds(175, 20, 165, 25);
+		panel.add(therapistText);
+		
+		JLabel patientLabel = new JLabel("Patient Name");
+		patientLabel.setBounds(10, 60, 100, 25);
+		panel.add(patientLabel);
+		
+		JTextField patientText = new JTextField(20);
+		patientText.setBounds(175, 60, 165, 25);
+		panel.add(patientText);
+		
 		
 		frame.setVisible(true);
 	}
